061c95c02a311c9cb9feeb1347a3145de37bede3
HanGyeol9931 <jn365@naver.com>
2022-09-07 17:13:38 +0900

    22.09.07

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const dot = require("dotenv").config();
 const config = {
   dev: {
     username: "admin",
-    password: "99319931",
+    password: "admin1234",
     database: "honeybee",
     // 사용을 한다면 이곳에 주소를 넣어주면 됩니다.
     dialect: "mysql",
