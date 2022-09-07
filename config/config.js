6d089de94a2c287e05b029c46a29d99a6fb007ed
HanGyeol9931 <jn365@naver.com>
2022-09-07 17:08:18 +0900

    22.09.07

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const dot = require("dotenv").config();
 // 데이터 베이스 접속에 필요한 설정값 객체
 const config = {
   dev: {
-    username: "honeybee",
+    username: "admin",
     password: "99319931",
     database: "honeybee",
     host: "127.0.0.1", // 여기에 만약 우리가 AWS RDS 쓰거나 지원 데이터 베이스 등등
